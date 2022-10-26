4d30b9f84e942643de26842bdfe3ca886178bd29
unknown <youssef.ojeil@hotmail.com>
2022-10-25 21:56:32 -0400

    set up initial questions for inquirer

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
+
+const questions = [
+    {
+        type: "input",
+        name: "fName",
+        message: "Please enter your name:"
+    },
+    
+    {
+        type: "input",
+        name: "id",
+        message: "Please enter your ID:"
+    },
+
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter your email"
+    }
+];
+
+
+
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        console.log(answers);
+    });
+}
+
+init();
