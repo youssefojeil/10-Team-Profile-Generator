251137462f863eb356fbe6dd36b21d5a0c577aa4
unknown <youssef.ojeil@hotmail.com>
2022-10-27 19:15:08 -0400

    Complete test suite for employee.test.js passing

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,54 @@
+const Employee = require("../lib/Employee");
+
+test("Can instantiate Employee instance", () => {
+    const e = new Employee();
+    expect(typeof(e)).toBe("object");
+});
+
+// TESTS FOR SETTERS //
+// testing set employee name
+test("can set name via constructor arguments", () => {
+    const employee = new Employee("Youssef");
+    expect(employee.name).toBe("Youssef");
+});
+
+// testing set employee id
+test("can set name id constructor arguments", () => {
+    
+    const employee = new Employee("Youssef", 15);
+    expect(employee.id).toBe(15);
+});
+
+// testing set employee email
+test("can set name email constructor arguments", () => {
+    const employee = new Employee("Youssef", 24, "test@email.com");
+    expect(employee.email).toBe("test@email.com");
+});
+
+// TESTS FOR GETTERS //
+
+// testing for getName() employee
+test("can get name via getName()", () => {
+    const employee = new Employee("Youssef");
+    expect(employee.getName()).toBe(employee.name);
+});
+
+// testing for getId() employee
+test("can get id via getId()", () => {
+    const employee = new Employee("Youssef", 199);
+    expect(employee.getId()).toBe(employee.id);
+});
+
+
+// testing for getEmail() employee
+test("can get id via getEmail()", () => {
+    const employee = new Employee("Youssef", 199, "email@test.com");
+    expect(employee.getEmail()).toBe(employee.email);
+});
+
+
+// testing for getRole() employee
+test("can get id via getRole()", () => {
+    const employee = new Employee("Youssef", 199, "email");
+    expect(employee.getRole()).toBe("Employee");
+});
